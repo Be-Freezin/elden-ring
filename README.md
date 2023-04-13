fdb99af6aaf6e0ba510dc6b1f6a5ad78feb08c50
Be-Freezin <brayf@live.com>
2023-04-13 18:07:34 -0500

    Working on Weapon Data
    Weapon data is rendering fine, i just have to work on some logic behind it.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Things i learned/struggled with:
 
 Making a reusable function that takes in state thats stored as an object and is able to change the values of the state.
 
+
--- elden-ring/src/components/characterdata/gear/Weapons.jsx
+++ elden-ring/src/components/characterdata/gear/Weapons.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Weapons = ({ weapons }) => {
+
+    const [selectedWeapon, setSelectedWeapon] = useState('')
+
+    const handleSelectChange = (event) =>{
+        const selectedIndex = event.target.selectedIndex
+        setSelectedWeapon(weapons[ selectedIndex ] )
+    }
+
 	const weaponsList = weapons.map((item, id) => {
 		return <option className="text-white" key={id} value={item.name}>
             {item.name}
         </option>
 	})
-console.log(weapons)
+console.log( selectedWeapon.requiredAttributes)
 	return (
 		<div>
 			<div className=" md:flex-row md:justify-evenly  mobile-container mt-6">
 				<div className="flex flex-col ">
 					<span className="small-txt">Left Hand 1</span>
-					<select className="dropdown-select" name="weaponsList" id="weaponsList">
+					<select
+						className="dropdown-select"
+						name="weaponsList"
+						id="weaponsList"
+						onChange={handleSelectChange}
+					>
+                        <option value=""></option>
 						{weaponsList}
 					</select>
-					<span className="small-txt">Req: S/D/I/F/A </span>
+					<span className="small-txt">
+						Req: {}/D/I/F/A{' '}
+					</span>
 				</div>
 				<div className="flex flex-col">
 					<span className="small-txt">Right Hand 1</span>
